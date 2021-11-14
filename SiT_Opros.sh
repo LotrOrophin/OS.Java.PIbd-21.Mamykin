a02c279262c47b35c3380b62b75b30ad27210f7d
LotrOrophin <55974379+LotrOrophin@users.noreply.github.com>
2021-11-14 22:23:48 +0400

    Delete SiT_Opros.sh

--- SiT_Opros.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone https://gitlab.com/romanov73/example-jpa.git
-cd example-jpa
-./gradlew build
-dnf update
-dnf install postgresql
-docker build -t sit -f dockerfile .
-systemctl stop postgresql
-docker run -it --name=containersit sit
